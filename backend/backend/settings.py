13b023d284981e8246c700e8fa7a3ab77195ea1b
vanxLord90 <dasaka.vishnu@gmail.com>
2023-09-06 11:44:37 -0400

    completed mongodb setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,14 +78,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'todo_list_table',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
