03b7d946c5cbbf04a72369e2f79a64f34290a470
vanxLord90 <dasaka.vishnu@gmail.com>
2023-09-25 12:14:24 -0400

    minor additions

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -3,6 +3,5 @@ from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title','description','due_date','priority','completed')
-
 admin.site.register(Todo, TodoAdmin)
 # Register your models here.
